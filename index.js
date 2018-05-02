3cc9fd17539690c3400cdc94f9cd005eb3cfd938
Sean Ruhland <sean.ruhland@gmail.com>
2018-05-02 15:14:06 -0700

    reducer function

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-
 {
-  type:'ADD_TODO',
+  type: 'ADD_TODO',
   todo: {
     id: 0,
     name: 'Learn Redux',
@@ -10,33 +9,55 @@
 
 {
   type: 'REMOVE_TODO',
-  id:0,
+  id: 0,
 }
 
 {
   type: 'TOGGLE_TODO',
-  id:0,
+  id: 0,
 }
 
 {
   type: 'ADD_GOAL',
   goal: {
     id: 0,
-    name: 'Run a marathon',
+    name: 'Run a Marathon'
   }
 }
 
 {
-  type:'REMOVE_GOAL',
-  id:0,
+  type: 'REMOVE_GOAL',
+  id: 0
 }
 
-function createStore () {
-  //the store should have four parts
-  //1. The state
-  //2. Get the state.
-  //3. Listen to changes on the state.
-  //4. Update the state
+/*
+Characteristics of a Pure Function
+1) They always return the same result if the same arguments are passed in.
+2) They depend only on the arguments passed into them.
+3) Never produce any side effects.
+*/
+
+// Reducer function
+function todos (state = [], action) {
+  if (action.type === 'ADD_TODO') {
+    return state.concat([action.todo])
+  } else if (action.type === 'REMOVE_TODO') {
+    return state.filter((todo) => todo.id !== action.id)
+  } else if (action.type === 'TOGGLE_TODO') {
+    return state.map((todo) => todo.id !== action.id ? todo :
+      Object.assign({}, todo, {complete:!todo.complete})
+    )
+  } else {
+    return state
+  }
+}
+
+function createStore (reducer) {
+  // The store should have four parts
+  // 1. The state
+  // 2. Get the state. (getState)
+  // 3. Listen to changes on the state. (subscribe)
+  // 4. Update the state (dispatch)
 
   let state
   let listeners = []
@@ -50,9 +71,29 @@ function createStore () {
     }
   }
 
+  const dispatch = (action) => {
+    state = reducer(state, action)
+    listeners.forEach((listener) => listener())
+  }
+
   return {
     getState,
-    subscribe
+    subscribe,
+    dispatch,
   }
 }
 
+const store = createStore(todos)
+
+const unsubuscribe = store.subscribe(() => {
+console.log('The new STATE is ', store.getState())
+})
+
+store.dispatch({
+  type: 'ADD_TODO',
+  todo: {
+    id: 1,
+    name: 'Learn Redux 100%',
+    complete: true,
+}
+})
